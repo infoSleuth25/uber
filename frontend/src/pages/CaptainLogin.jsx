b49b999caa41e4952fd283c1a4d7cc7540d2cd01
Siddhesh <siddheshborse25@gmail.com>
2025-03-06 17:05:41 +0530

    Started with frontend

---
+++ frontend/src/pages/CaptainLogin.jsx
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+const CaptainLogin = () => {
+  const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [captainData,setCaptainData] = useState({});
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        setCaptainData({
+            email : email,
+            password : password
+        })
+        setEmail('');
+        setPassword('');
+    }
+  return (
+    <div className='p-7 flex flex-col h-screen justify-between '>
+            <div>
+                <img className='w-18 mb-10' src="/uberdriver.png" alt="" />
+                <form onSubmit={(e)=>{
+                    submitHandler(e);
+                }}>
+                    <h3 className='text-lg font-medium mb-2'>What's your email</h3>
+                    
+                    <input className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base' 
+                    value = {email}
+                    onChange={(e)=>{
+                        setEmail(e.target.value)
+                    }}
+                    required 
+                    type="email" 
+                    placeholder='email@example.com' 
+                    />
+                    
+                    <h3 className='text-lg font-medium mb-2'>Enter your password</h3>
+                    <input 
+                    className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base' 
+                    value = {password}
+                    onChange={(e)=>{
+                        setPassword(e.target.value)
+                    }}
+                    type="password" 
+                    required 
+                    placeholder='password' 
+                    />
+
+                    
+                    <button className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2  w-full text-lg placeholder:text-base'>Login</button>
+
+                    <p className='text-center'>Join a fleet? <Link to='/captain-signup' className='text-blue-600'>Register as a Captain</Link></p>
+                </form>
+            </div>
+            <div>
+                <Link to='/login' className='bg-[#d5662d] flex items-center justify-center text-white font-semibold mb-7 rounded px-4 py-2  w-full text-lg placeholder:text-base'>Sign in as User</Link>
+            </div> 
+        </div>
+  )
+}
+
+export default CaptainLogin
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='bg-[url("/homeimage.jpeg")] bg-center bg-cover h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
+            <img className='w-16 ml-8' src="https://freelogopng.com/images/all_img/1659768779uber-logo-white.png" alt="" />
+            <div className='bg-white pb-7 py-5 px-4'>
+                <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
+                <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
